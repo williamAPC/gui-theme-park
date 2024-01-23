3a7e9422f02307a35627a4cb5c428033c66b3bc5
williamAPC <59400443+williamAPC@users.noreply.github.com>
2024-01-24 00:04:33 +0100

    Update variables.tf

--- .terraform-iac/variables.tf
+++ .terraform-iac/variables.tf
@@ -1,4 +1,4 @@
-variable "aws_region" {
+aultvariable "aws_region" {
   type        = string
   description = "Region AWS"
   default     = "eu-west-3"
@@ -66,7 +66,7 @@ variable "ami_type" {
 variable "account_id" {
   type        = string
   description = "AWS Account ID"
-  sensitive   = "285679047437"
+  default     = "285679047437"
 }
 
 variable "iam_list" {
