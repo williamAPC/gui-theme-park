e7b121295676e076539416919d4c07bfed95e3d1
williamAPC <59400443+williamAPC@users.noreply.github.com>
2024-03-22 23:55:19 +0100

    commit modif again for newline error

--- .terraform-iac/rds/main.tf
+++ .terraform-iac/rds/main.tf
@@ -18,11 +18,9 @@ module "rds" {
   storage_encrypted    = false
 
   db_name  = var.db_name
+  username = _{_DBUSERNAME_}
+  password = _{_DBPASSWORD_}
 
-  username = _{_DBUSERNAME_}_
-
-  password = _{_DBPASSWORD_}_
-  
   port     = 3306
 
   manage_master_user_password = false
