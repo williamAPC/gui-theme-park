d2fd6c3d9444b43293da37576f194ce34115cfcd
williamAPC <59400443+williamAPC@users.noreply.github.com>
2024-03-22 23:48:52 +0100

    modif variables for DB

--- .terraform-iac/rds/main.tf
+++ .terraform-iac/rds/main.tf
@@ -18,8 +18,8 @@ module "rds" {
   storage_encrypted    = false
 
   db_name  = var.db_name
-  username = _{_DB_USERNAME_}_
-  password = _{_DB_PASSWORD_}_
+  username = _{_DBUSERNAME_}_
+  password = _{_DBPASSWORD_}_
   port     = 3306
 
   manage_master_user_password = false
