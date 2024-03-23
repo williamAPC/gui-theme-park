c5be06093d2e6e5b221928a90ab04804999e8194
williamAPC <59400443+williamAPC@users.noreply.github.com>
2024-03-23 03:02:56 +0100

    commit

--- .terraform-iac/rds/variables.tf
+++ .terraform-iac/rds/variables.tf
@@ -18,14 +18,14 @@ variable "db_name" {
   type = string
 }
 
-#variable "db_username" {
- # type = string
-#}
+variable "db_username" {
+  type = string
+}
 
-#ariable "db_password" {
-# type      = string
- # sensitive = true
-#}
+ariable "db_password" {
+ type      = string
+  sensitive = true
+}
 
 variable "vpc_id" {
   description = "VPC Id"
