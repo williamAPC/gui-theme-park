88a9102257f5d0879ad375b8c9eb9c719877ce30
williamAPC <59400443+williamAPC@users.noreply.github.com>
2024-03-25 12:28:28 +0100

    commit

--- .terraform-iac/rds/main.tf
+++ .terraform-iac/rds/main.tf
@@ -2,7 +2,7 @@
 resource "aws_db_subnet_group" "mariadb-subnets" {
     name        = "mariadb-subnets"
     description = "Amazon RDS subnet group"
-    subnet_ids  = [var.private_subnets[0].id, var.private_subnets[1].id]
+    subnet_ids  = module.vpc.public_subnets
 }
 
 
