4fe6bb2a8d4f6c3d001f7cffc1fd3aae48eb5d9e
williamAPC <59400443+williamAPC@users.noreply.github.com>
2024-03-25 22:38:14 +0100

    commit

--- .terraform-iac/rds/outputs.tf
+++ .terraform-iac/rds/outputs.tf
@@ -1,5 +1,17 @@
 
+
+
+
+
+
+
+
+
+
+
+/*
 # output "db_instance_endpoint" {
 # description = "RDS endpoint"
- # value       = aws_db_instance.tpr-mariadb.endpoint
+# value       = aws_db_instance.tpr-mariadb.endpoint
 #}
+*/
