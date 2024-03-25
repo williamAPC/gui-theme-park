da05be1093d5eaabd6280d1d0fb19c3e79518b39
williamAPC <59400443+williamAPC@users.noreply.github.com>
2024-03-25 15:19:14 +0100

    commit

--- .terraform-iac/network/main.tf
+++ .terraform-iac/network/main.tf
@@ -9,10 +9,8 @@ module "vpc" {
 
   cidr                    = var.vpc_cidr
   azs                     = data.aws_availability_zones.available.names
-  private_subnets1         = var.private_subnets1
-  public_subnets1          = var.public_subnets1
-  private_subnets2        = var.private_subnets2
-  public_subnets2         = var.public_subnets2
+  private_subnets         = var.private_subnets
+  public_subnets         = var.public_subnets
   create_igw              = true
   enable_nat_gateway      = false
   map_public_ip_on_launch = true
