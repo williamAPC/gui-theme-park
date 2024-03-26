51050e606fb52cbc70f0e7b8fc56e63eadc0c39e
williamAPC <59400443+williamAPC@users.noreply.github.com>
2024-03-26 11:15:44 +0100

    comm

--- .terraform-iac/rds/outputs.tf
+++ .terraform-iac/rds/outputs.tf
@@ -1,17 +1,4 @@
-
-
-
-
-
-
-
-
-
-
-
-/*
-# output "db_instance_endpoint" {
-# description = "RDS endpoint"
-# value       = aws_db_instance.tpr-mariadb.endpoint
-#}
-*/
+output "db_instance_endpoint" {
+ description = "RDS endpoint"
+ value       = aws_db_instance.tpr-mariadb.endpoint
+}
