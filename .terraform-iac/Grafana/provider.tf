0040e0cdb4ee2df40b364881c6fd47857f266fda
williamAPC <59400443+williamAPC@users.noreply.github.com>
2024-03-26 19:10:29 +0100

    cooo

--- .terraform-iac/Grafana/provider.tf
+++ .terraform-iac/Grafana/provider.tf
@@ -1,10 +1,10 @@
-terraform {
+#terraform {
   #backend "s3" {
   #  bucket =  "guitests3bucket"
    # key = "grafanastate/terraform.tfstate"
 #       region can't be set with variables.tf here, should be set with environment variable AWS_REGION
-    region = "eu-west-3"
-  }
+ #  region = "eu-west-3"
+#  }
 
   required_providers {
     aws = {
